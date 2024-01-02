276d988e406e29ddb0e8af1da4064a110236a6e6
ilias <ilias.bidane@student.uantwerpen.be>
2024-01-02 21:51:38 +0100

    more parameters

--- PSAP_ILS.py
+++ PSAP_ILS.py
@@ -12,8 +12,10 @@ TIME_WINDOW = 60 * 6
 
 
 # ============ITERATED LOCAL SEARCH================
-def solve_with_precedence_constraints_ILS(movements: list, precedence: dict, max_time: int, epochs,
+def solve_with_precedence_constraints_ILS(movements: list, precedence: dict, max_time: int,
                                           iteration_time_limit=4,
+                                          neighbor_affected_movements=6, neighbor_deviation_scale=40,
+                                          homebase_affected_movements=4, homebase_deviation_scale=40,
                                           time_interval=5, vessel_time_window=60 * 6):
     # start the timer
     start_time = time.time()
@@ -34,8 +36,8 @@ def solve_with_precedence_constraints_ILS(movements: list, precedence: dict, max
         while time.time() - iteration_start_time < iteration_time_limit:
             # generate a new solution that can escape the local optimum
             new_solution = initial_solution.copy()
-            new_solution = generate_neighbor_solution(new_solution, affected_movements=6,
-                                                      deviation_scale=40,
+            new_solution = generate_neighbor_solution(new_solution, affected_movements=neighbor_affected_movements,
+                                                      deviation_scale=neighbor_deviation_scale,
                                                       time_interval=time_interval)
 
             if obj_func(new_solution, precedence) < obj_func(initial_solution, precedence):
@@ -51,35 +53,23 @@ def solve_with_precedence_constraints_ILS(movements: list, precedence: dict, max
             best_solution = initial_solution.copy()
             homebase = initial_solution.copy()
             # perturb the solution
-            homebase = generate_neighbor_solution(homebase, affected_movements=4,
-                                                  deviation_scale=40,
+            homebase = generate_neighbor_solution(homebase, affected_movements=homebase_affected_movements,
+                                                  deviation_scale=homebase_deviation_scale,
                                                   time_interval=time_interval)
         else:
             initial_solution = homebase.copy()
             initial_obj_val = obj_func(initial_solution, precedence)
 
-        # plot
-        plt.plot(obj_val)
-        plt.show()
-
-        # if abs(new_obj_val - initial_obj_val) < 0.000001:
-        #     stopping_condition_counter += 1
-        #     if stopping_condition_counter == 10 * neighborhood_size:
-        #         initial_solution = {m: m.optimal_time for m in movements}
-        #         initial_obj_val = obj_func(initial_solution)
-        #         e = 0
-        #         attempt += 1
-    #
-
-    if validate_solution(initial_solution, vessel_time_window, print_errors=True):
-        return initial_solution, initial_obj_val
+    if validate_solution(best_solution, vessel_time_window, print_errors=True):
+        return best_solution, obj_func(best_solution)
     else:
         return None, None
 
 
-def solution_generating_procedure(movements: list, l, t, epochs=200, neighborhood_size=4, t0=100,
-                                  alpha=.8, neighbor_deviation_scale=40, affected_movements=3, time_interval=5,
-                                  vessel_time_window=60 * 6):
+def solution_generating_procedure(movements: list, l, t, iteration_time_limit=4,
+                                  neighbor_deviation_scale=40, neighbor_affected_movements=6,
+                                  homebase_deviation_scale=40, homebase_affected_movements=3,
+                                  time_interval=5, vessel_time_window=60 * 6):
     # movements is a list of movements
     # sort the movements by time
     sorted_movements = sorted(movements, key=lambda x: x.optimal_time)
@@ -94,7 +84,11 @@ def solution_generating_procedure(movements: list, l, t, epochs=200, neighborhoo
         problem_subset = fixed_movements + movements_subset
         # solve the problem with the new subset and the precedence constraints
         solution, obj_val = solve_with_precedence_constraints_ILS(problem_subset, precedence, max_time=t,
-                                                                  epochs=epochs,
+                                                                  iteration_time_limit=iteration_time_limit,
+                                                                  neighbor_affected_movements=neighbor_affected_movements,
+                                                                  neighbor_deviation_scale=neighbor_deviation_scale,
+                                                                  homebase_affected_movements=homebase_affected_movements,
+                                                                  homebase_deviation_scale=homebase_deviation_scale,
                                                                   time_interval=time_interval,
                                                                   vessel_time_window=vessel_time_window)
 
@@ -126,27 +120,38 @@ def solution_generating_procedure(movements: list, l, t, epochs=200, neighborhoo
     return solution, obj_val, None
 
 
-def generate_parameters(epochs_rng):
-    epochs = rd.randint(epochs_rng[0], epochs_rng[1])
-    return epochs
+def generate_parameters(neighbor_deviation_scale_rng, neighbor_affected_movements_rng, homebase_deviation_scale_rng,
+                        homebase_affected_movements_rng):
+    # generate the parameters
+    neighbor_deviation_scale = rd.randint(neighbor_deviation_scale_rng[0], neighbor_deviation_scale_rng[1])
+    neighbor_affected_movements = rd.randint(neighbor_affected_movements_rng[0], neighbor_affected_movements_rng[1])
+    homebase_deviation_scale = rd.randint(homebase_deviation_scale_rng[0], homebase_deviation_scale_rng[1])
+    homebase_affected_movements = rd.randint(homebase_affected_movements_rng[0], homebase_affected_movements_rng[1])
+
+    # make sure that deviation is in intervals of 5
+    neighbor_deviation_scale = round(neighbor_deviation_scale / 5) * 5
+    homebase_deviation_scale = round(homebase_deviation_scale / 5) * 5
+
+    return neighbor_deviation_scale, neighbor_affected_movements, homebase_deviation_scale, homebase_affected_movements
 
 
 if __name__ == '__main__':
     sol_found = False
-    instance = 1
+    instance = 14
     valid_solutions = []
     objective_values = []
 
-    df = pd.DataFrame(columns=['instance', 'number of movements', 'median delay', 'average delay', 'epochs', 'obj_val',
-                               'neighborhood_size', 't0', 'alpha', 'neighbor_deviation_scale', 'affected_movements',
-                               'time_interval', 'vessel_time_window', 'valid_solution'])
+    df = pd.DataFrame(columns=['instance', 'number of movements', 'median delay', 'average delay', 'obj_val',
+                               'iteration_time_limit', 'neighbor_deviation_scale',
+                               'neighbor_affected_movements', 'homebase_deviation_scale', 'homebase_affected_movements',
+                               'time_interval', 'vessel_time_window', 'solution_found'])
 
     df_instance = pd.DataFrame(columns=['instance', 'number_of_movements', 'number_of_vessels', 'average_headway',
                                         'std_dev_headway', 'spread', 'average_time_between_movements',
                                         'average_travel_time'])
 
     time_window = 60 * 6
-    time_interval = 5
+    time_interval = 10
     while instance < 101:
         print("=====================================")
         print("Instance: ", instance)
@@ -161,13 +166,23 @@ if __name__ == '__main__':
         print("Objective value initial solution: ", obj_func(initial_solution))
 
         # run the solution generating procedure 10 times for each instance and save the results
-        for _ in range(3):
-            epochs = generate_parameters(epochs_rng=[100, 100])
+        for _ in range(5):
+            neighbor_deviation_scale, neighbor_affected_movements, homebase_deviation_scale, homebase_affected_movements \
+                = generate_parameters(neighbor_deviation_scale_rng=[30, 50], neighbor_affected_movements_rng=[5, 7],
+                                      homebase_deviation_scale_rng=[20, 45], homebase_affected_movements_rng=[3, 5])
 
-            initial_solution, obj_val, prev_initial_solution = solution_generating_procedure(result_list, 3, 5,
-                                                                                             epochs=epochs,
-                                                                                             time_interval=5,
-                                                                                             vessel_time_window=TIME_WINDOW)
+            print([neighbor_deviation_scale, neighbor_affected_movements, homebase_deviation_scale,
+                   homebase_affected_movements])
+
+            initial_solution, obj_val, prev_initial_solution = \
+                solution_generating_procedure(result_list, 3, 5,
+                                              iteration_time_limit=4,
+                                              neighbor_deviation_scale=neighbor_deviation_scale,
+                                              neighbor_affected_movements=neighbor_affected_movements,
+                                              homebase_deviation_scale=homebase_deviation_scale,
+                                              homebase_affected_movements=homebase_affected_movements,
+                                              time_interval=time_interval,
+                                              vessel_time_window=time_window)
 
             if initial_solution is not None:
                 # set the movement scheduled to the result of the solution generating procedure
@@ -176,28 +191,31 @@ if __name__ == '__main__':
                 print("Solution", _, " found for instance", instance, "(", len(initial_solution), ")")
                 obj_val = obj_func(initial_solution)
                 print("Objective value: ", obj_val)
-                # df.loc[len(df.index)] = [instance, len(initial_solution),
-                #                          np.median([abs(m.get_delay()) for m in initial_solution.keys()]),
-                #                          np.mean([abs(m.get_delay()) for m in initial_solution.keys()]),
-                #                          epochs, obj_val, neighborhood_size, t0, alpha, neighbor_deviation_scale,
-                #                          affected_movements, TIME_INTERVAL, TIME_WINDOW, 1]
+
+                df.loc[len(df.index)] = [instance, len(initial_solution),
+                                         np.median([abs(m.get_delay()) for m in initial_solution.keys()]),
+                                         np.mean([abs(m.get_delay()) for m in initial_solution.keys()]),
+                                         obj_val, 4, neighbor_deviation_scale, neighbor_affected_movements,
+                                         homebase_deviation_scale, homebase_affected_movements, TIME_INTERVAL,
+                                         TIME_WINDOW, 1]
+                sol_found += 1
             else:
                 print("No solution found for instance", instance)
                 for m, t in prev_initial_solution.items():
                     m.set_scheduled_time(t)
                 obj_val = obj_func(prev_initial_solution)
-
-                # df.loc[len(df.index)] = [instance, len(prev_initial_solution),
-                #                          np.median([abs(m.get_delay()) for m in prev_initial_solution.keys()]),
-                #                          np.mean([abs(m.get_delay()) for m in prev_initial_solution.keys()]),
-                #                          epochs, obj_val, neighborhood_size, t0, alpha, neighbor_deviation_scale,
-                #                          affected_movements, TIME_INTERVAL, TIME_WINDOW, 0]
+                df.loc[len(df.index)] = [instance, len(prev_initial_solution),
+                                         np.median([abs(m.get_delay()) for m in prev_initial_solution.keys()]),
+                                         np.mean([abs(m.get_delay()) for m in prev_initial_solution.keys()]),
+                                         obj_val, 4, neighbor_deviation_scale, neighbor_affected_movements,
+                                         homebase_deviation_scale, homebase_affected_movements, TIME_INTERVAL,
+                                         TIME_WINDOW, 0]
 
         instance += 1
 
-        # try:
-        #     df.to_excel('results/ILS/output_100e.xlsx', index=False)
-        # except PermissionError:
-        #     print("Please close the file output.xlsx and try again")
+        try:
+            df.to_excel('results/ILS/output_correction_100e_ctd.xlsx', index=False)
+        except PermissionError:
+            print("Please close the file output.xlsx and try again")
 
-        print("solutions found: ", len(df.index))
+        print("solutions found: ", sol_found, "/", len(df.index))
